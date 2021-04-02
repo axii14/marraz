82937d765697aa771d36bcdd9cceb747c5028b04
axii14 <64701220+axii14@users.noreply.github.com>
2021-04-02 19:39:29 +0200

    Create main2.js

---
+++ main2.js
@@ -0,0 +1,32 @@
+function rob() {
+    var kontra;
+    var zuzena="a1";
+    kontra=prompt('Webgunera sartu ahal izateko pasahitza sartu ezazu mesedez','');
+    if (kontra==zuzena){
+      alert('Zorionak! You are a valuable member of the MSU (Marraz Suffering Universe)! Click on the following link to proceed!');
+      window.location="html/BATXI1/marraz1.html";
+    } else {
+        window.location="notvip.html";
+        }}
+
+        function xan() {
+    var kontra;
+    var zuzena="a2";
+    kontra=prompt('Webgunera sartu ahal izateko pasahitza sartu ezazu mesedez','');
+    if (kontra==zuzena){
+      alert('Zorionak! You are a valuable member of the MSU (Marraz Suffering Universe)! Click on the following link to proceed!');
+      window.location="html/BATXI1/marraz1.html";
+    } else {
+        window.location="notvip.html";
+        }}
+
+        function axi() {
+    var kontra;
+    var zuzena="a3";
+    kontra=prompt('Webgunera sartu ahal izateko pasahitza sartu ezazu mesedez','');
+    if (kontra==zuzena){
+      alert('Zorionak! You are a valuable member of the MSU (Marraz Suffering Universe)! Click on the following link to proceed!');
+      window.location="html/BATXI1/marraz1.html";
+    } else {
+        window.location="notvip.html";
+        }}
