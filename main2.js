e9156233912138ea60003eb65a629c8f0e6d8825
axii14 <64701220+axii14@users.noreply.github.com>
2021-04-11 20:03:17 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -3,7 +3,7 @@ function rob() {
     var zuzena="a1";
     kontra=prompt('Webgunera sartu ahal izateko pasahitza sartu ezazu mesedez','');
     if (kontra==zuzena){
-      alert('Zorionak! You are a valuable member of the MU (Marraz Universe)! Click on the following link to proceed!');
+      alert('Kaixo Roberto!\nOngi etorri marraz-eko webgunera\nYou are a valuable member of the MU (Marraz Universe)!\nClick below to proceed!');
       window.location="html/BATXI1/marraz1.html";
     } else {
         window.location="notvip.html";
@@ -14,7 +14,7 @@ function rob() {
     var zuzena="a2";
     kontra=prompt('Webgunera sartu ahal izateko pasahitza sartu ezazu mesedez','');
     if (kontra==zuzena){
-      alert('Zorionak! You are a valuable member of the MU (Marraz Universe)! Click on the following link to proceed!');
+      alert('Kaixo Xanti!\nOngi etorri marraz-eko webgunera\nYou are a valuable member of the MU (Marraz Universe)!\nClick below to proceed!');
       window.location="html/BATXI1/marraz1.html";
     } else {
         window.location="notvip.html";
